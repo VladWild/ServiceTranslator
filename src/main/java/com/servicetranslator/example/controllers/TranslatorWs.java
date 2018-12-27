76a4ced4c67b84f23ee21be02836e90fb76ab852
v.v.gusev <v.v.gusev@tinkoff.ru>
2018-12-27 11:55:56 +0300

    первая рабочая версия проекта

--- src/main/java/com/servicetranslator/example/controllers/TranslatorWs.java
+++ src/main/java/com/servicetranslator/example/controllers/TranslatorWs.java
@@ -2,18 +2,17 @@ package com.servicetranslator.example.controllers;
 
 import com.servicetranslator.example.dto.Response;
 import org.springframework.stereotype.Controller;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TranslatorWs {
 
-    @GET
-    @Path("/")
-    public Response getTranslatedText(){
-        System.out.println("Зашел");
-        return new Response();
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 
 }

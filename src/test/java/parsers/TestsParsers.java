2829b05db6d3a0c575060a327215f0dd922a0845
v.v.gusev <v.v.gusev@tinkoff.ru>
2018-12-28 12:32:56 +0300

    добавлен тест парсера

--- src/test/java/parsers/TestsParsers.java
+++ src/test/java/parsers/TestsParsers.java
@@ -6,23 +6,24 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertArrayEquals;
+import static junit.framework.TestCase.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestsParsers {
 
     @Test
     public void testSimpleParser(){
-        String[] arrayWordsForOutput = {"привет", "мир", "слово", "привет", "пока"};
+        List<String> listWordsForOutput = Arrays.asList("привет", "мир", "слово", "привет", "пока");
 
         String text = "привет мир слово привет пока";
 
         Parser parser = new SimpleParser();
         List<String> arrayWord = parser.getWords(text);
 
-        assertArrayEquals(arrayWordsForOutput, arrayWordsForOutput);
+        assertEquals(listWordsForOutput, arrayWord);
     }
 
 }
